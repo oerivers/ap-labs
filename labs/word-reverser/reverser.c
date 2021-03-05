dc06da76af4f614dd282343b3a4c71cd8ec95e47
root <root@DESKTOP-VTRCCV0.localdomain>
2021-03-04 19:42:13 -0600

    Solved Reverser
    Signed-off-by: root <root@DESKTOP-VTRCCV0.localdomain>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    // Place your magic here
-    return 0;
+static int n;
+
+void reverse(int length, char word[]){
+    
+    n = 0;
+    int i;
+    char tmp;
+
+    for(i = 0; i < length/2; i++)
+    {
+        tmp = word[i];
+        word[i] = word[length - i - 1];
+        word[length - i - 1] = tmp;
+    }
+
+    printf("%s\n", word);
+}
+
+int main(int argc, char *argv)
+{
+    char c;
+    char word[100];
+
+    n = 0;
+
+    while((c = getchar()) != EOF)
+    {
+        word[n++] = c;
+        if(c == '\n')
+        {
+            reverse(n, word);
+            memset(word, 0, 100);
+        }
+    }
+return 0;
 }
