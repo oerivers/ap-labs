06a92e1c977050ffc2b52f90928afbe7dadb35c0
Oscar Rios <oscarrm96@hotmail.com>
2021-03-23 22:49:35 -0600

    Solved-Str-Lib
    Signed-off-by: Oscar Rios <oscarrm96@hotmail.com>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,32 @@
+//Oscar Eduardo Rios Macriz - A01229111
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    return 0;
+int mystrlen(char *);
+char *mystradd(char *,char *);
+int mystrfind(char *,char *);
+
+int main(int argc, char *argv[]) {
+    char *string = (char *)malloc(sizeof(char *)),
+         *string2 = (char *)malloc(sizeof(char *)),
+         *r = (char *)malloc(sizeof(char *));
+    string = argv[2];
+    string2 = argv[3];
+    if (mystrfind(argv[1],"-add")==0 && argc == 4)
+    {
+        printf("Initial Length\t: %d\n", mystrlen(string));
+        mystradd(string,string2);
+        printf("The new string\t: %s\n", string);
+        printf("The new length\t: %d\n", mystrlen(string));
+        return 0;
+    }
+    else if(mystrfind(argv[1],"-find")==0 && argc == 4)
+    {
+        printf("[%s] the string found was at [%d] position\n", string2,mystrfind(string,string2));
+    }
+    else
+    {
+        return 0;
+    }
+    
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,44 @@
-
+//Oscar Eduardo Rios Macriz - A01229111
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int count = 0;
+    while (str[count] != '\0')
+    {
+        count++;
+    }
+    return count;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int count       = mystrlen(origin),
+        additionlen = mystrlen(addition);
+    for(int i = 0; i < additionlen; i++)
+    {
+        *(origin+count++) = *(addition+i);
+    }
+    *(origin+count) = '\0';
+    return origin;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int count       = 0,
+        limit       = mystrlen(substr),
+        originlen   = mystrlen(origin);
+    for(int i = 0; i < originlen; i++)
+    {
+        if(*(origin+i) == *(substr+count))
+        {
+            count++;
+        }
+        else 
+        {
+            i = i - count + 1;
+            count = 0;
+        }
+        if(count==limit){
+            return i - count + 1;
+        }
+    }
+    return -1;
 }
